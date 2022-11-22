77ccae3a80517cf4ef6202f3d88b49a3308f6e8b
Abhinaya2022 <Abhinaya2022@gmail.com>
2022-11-22 15:08:39 +0530

    patientA added in index

--- PatientMVC/PatientMVC/Controllers/AppointmentController.cs
+++ PatientMVC/PatientMVC/Controllers/AppointmentController.cs
@@ -14,6 +14,7 @@ namespace PatientMVC.Controllers
         public IActionResult Index()
         {
             Patient patient=new Patient();
+            Patient patientA=new Patient();
             return View();
         }
         [HttpGet]
